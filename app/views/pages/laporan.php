4bf64e235c4a36e3eeda00eadd0cc5f84e412285
atadityas13 <at.adityas13@gmail.com>
2026-03-07 16:14:46 +0700

    fix: Adjust footer styles and spacing for improved layout in laporan page

--- app/views/pages/laporan.php
+++ app/views/pages/laporan.php
@@ -613,13 +613,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                         <td style="text-align: center; vertical-align: middle; padding-right: 4px;">
                             <h2 style="margin: 0; font-size: 16px; font-weight: bold; font-family: Times New Roman, serif; letter-spacing: 1.8px;">KEMENTERIAN AGAMA REPUBLIK INDONESIA</h2>
                             <h1 style="margin: 2px 0; font-size: 25px; line-height: 1.05; font-weight: bold; font-family: Times New Roman, serif; letter-spacing: 2.4px;">MTsN 11 MAJALENGKA</h1>
-                            <p style="margin: 2px 0 0 0; font-size: 11px; font-style: italic; font-family: Times New Roman, serif;">Kp. Sindanghurip Desa Maniis Kec. Cingambul Kab. Majalengka, 45467.</p>
-                            <p style="margin: 0; font-size: 11px; font-style: italic; font-family: Times New Roman, serif;">Telp. (0233) 3600020  E-mail: mtsn11majalengka@gmail.com </p>
+                            <p style="margin: 3px 0 0 0; font-size: 11px; font-style: italic; font-family: Times New Roman, serif; letter-spacing: 0.5px;">Kp. Sindanghurip Desa Maniis Kec. Cingambul Kab. Majalengka, 45467.</p>
+                            <p style="margin: 0; font-size: 11px; font-style: italic; font-family: Times New Roman, serif; letter-spacing: 0.5px;">Telp. (0233) 3600020  E-mail: mtsn11majalengka@gmail.com </p>
                         </td>
                     </tr>
                 </table>
                 
-                <div style="height: 3px; border-top: 2px solid #000; border-bottom: 1px solid #000; margin: 6px 0 10px 0;"></div>
+                <div style="height: 1px; border-top: 2px solid #000; border-bottom: 1px solid #000; margin: 6px 0 10px 0;"></div>
                 
                 <!-- Title -->
                 <h2 style="text-align: center; margin: 2px 0 0 0; font-size: 16px; font-weight: bold; letter-spacing: 0.3px;">TRANSKRIP NILAI</h2>
