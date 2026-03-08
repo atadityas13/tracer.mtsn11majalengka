cac96016ea06ea2c8ccef4696a6fc14636bd93fa
atadityas13 <at.adityas13@gmail.com>
2026-03-08 23:42:19 +0700

    style: Add detailed file header documentation for Data Alumni Page

--- index.php
+++ index.php
@@ -1,4 +1,44 @@
 <?php
+/**
+ * ========================================================
+ * TRACER MTSN 11 MAJALENGKA
+ * ========================================================
+ * 
+ * Sistem Manajemen Data Nilai Siswa
+ * MTsN 11 Majalengka, Kabupaten Majalengka, Jawa Barat
+ * 
+ * File: Data Alumni Page
+ * Deskripsi: Halaman view data alumni dengan modal nilai rapor, UAM, dan ijazah
+ * 
+ * @package    TRACER-MTSN11
+ * @author     MTsN 11 Majalengka Development Team
+ * @copyright  2026 MTsN 11 Majalengka. All rights reserved.
+ * @license    Proprietary License
+ * @version    1.0.0
+ * @since      2026-01-01
+ * @created    2026-03-06
+ * @modified   2026-03-06
+ * 
+ * Features:
+ * - List data alumni dengan nama, NISN, tahun lulus
+ * - Modal view nilai per semester (rapor 1-5, UAM, ijazah)
+ * - Kalkulasi nilai ijazah otomatis dari rapor + UAM
+ * - Backfill nama alumni dari siswa untuk data migrasi lama
+ * - Pagination dan search alumni
+ * 
+ * DISCLAIMER:
+ * Software ini dikembangkan khusus untuk MTsN 11 Majalengka.
+ * Dilarang keras menyalin, memodifikasi, atau mendistribusikan
+ * tanpa izin tertulis dari MTsN 11 Majalengka.
+ * 
+ * CONTACT:
+ * Website: https://mtsn11majalengka.sch.id
+ * Email: mtsn11majalengka@gmail.com
+ * Phone: (0233) 3600020
+ * 
+ * ========================================================
+ */
+<?php
 
 declare(strict_types=1);
 
